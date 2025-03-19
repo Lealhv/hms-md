e2454a52cd75a4df38d0b46520315994790e11f1
lea <lea@gmail.com>
2025-03-19 15:10:46 +0200

    creat service and controller

--- Back/config/database.php
+++ Back/config/database.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root'@'localhost";
-$password = "new_password";
+$username = "root";
+$password = ""; // Try with an empty password
 $dbname = "isgulaorg_101";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

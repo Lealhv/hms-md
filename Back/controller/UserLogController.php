b67c79741d5747b68a018304c47ab25a05939844
sarah-frankel <frankels024@gmail.com>
2025-03-20 14:10:15 +0200

    server work

--- Back/controller/UserLogController.php
+++ Back/controller/UserLogController.php
@@ -1,69 +1,71 @@
 <?php
-class UserController {
-    private $db;
 
-    public function __construct($database) {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type");
-        
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-            // אם הבקשה היא OPTIONS, פשוט מחזירים תשובה ריקה
-            exit(0);
+class UserLogController {
+    private $userLogList = [];
+
+    // Create
+    public function createLog($Lg_user, $Lg_status, $Lg_timestmpin, $Lg_timestmpiout) {
+        $userLog = new UserLog();
+        $userLog->Lg_user = $Lg_user;
+        $userLog->Lg_status = $Lg_status;
+        $userLog->Lg_timestmpin = $Lg_timestmpin;
+        $userLog->Lg_timestmpiout = $Lg_timestmpiout;
+        $this->userLogList[] = $userLog;
+        return $userLog;
+    }
+
+    // Read
+    public function readLog($index) {
+        if (isset($this->userLogList[$index])) {
+            $data = $this->userLogList[$index];
+            echo json_encode($data);
+        } else {
+            echo json_encode(null);
         }
-        $this->db = $database;
     }
-
-    public function create($lg_user, $lg_status, $lg_timestmpin, $lg_timestmpiout) {
-        $stmt = $this->db->prepare("INSERT INTO users (Lg_user, Lg_status, Lg_timestmpin, Lg_timestmpiout) VALUES (?, ?, ?, ?)");
-        $stmt->execute([$lg_user, $lg_status, $lg_timestmpin, $lg_timestmpiout]);
+    // Update
+    public function updateLog($index, $Lg_user, $Lg_status, $Lg_timestmpin, $Lg_timestmpiout) {
+        if (isset($this->userLogList[$index])) {
+            $this->userLogList[$index]->Lg_user = $Lg_user;
+            $this->userLogList[$index]->Lg_status = $Lg_status;
+            $this->userLogList[$index]->Lg_timestmpin = $Lg_timestmpin;
+            $this->userLogList[$index]->Lg_timestmpiout = $Lg_timestmpiout;
+            return $this->userLogList[$index];
+        }
+        return null;
     }
-
-    public function read($id) {
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch(); 
+    // Update partial
+    public function updatePartialLog($index, $Lg_user = null, $Lg_status = null, $Lg_timestmpin = null, $Lg_timestmpiout = null) {
+        if (isset($this->userLogList[$index])) {
+            if ($Lg_user !== null) {
+                $this->userLogList[$index]->Lg_user = $Lg_user;
+            }
+            if ($Lg_status !== null) {
+                $this->userLogList[$index]->Lg_status = $Lg_status;
+            }
+            if ($Lg_timestmpin !== null) {
+                $this->userLogList[$index]->Lg_timestmpin = $Lg_timestmpin;
+            }
+            if ($Lg_timestmpiout !== null) {
+                $this->userLogList[$index]->Lg_timestmpiout = $Lg_timestmpiout;
+            }
+            return $this->userLogList[$index];
+        }
+        return null;
     }
-
-    public function update($id, $lg_user, $lg_status, $lg_timestmpin, $lg_timestmpiout) {
-        $stmt = $this->db->prepare("UPDATE users SET Lg_user = ?, Lg_status = ?, Lg_timestmpin = ?, Lg_timestmpiout = ? WHERE id = ?");
-        $stmt->execute([$lg_user, $lg_status, $lg_timestmpin, $lg_timestmpiout, $id]);
-    }
-    public function PartialUpdate($id, $userData) {
-        $fields = [];
-        $params = [];
     
-        if (isset($userData['lg_user'])) {
-            $fields[] = "Lg_user = ?";
-            $params[] = $userData['lg_user'];
+    // Delete
+    public function deleteLog($index) {
+        if (isset($this->userLogList[$index])) {
+            unset($this->userLogList[$index]);
+            return true;
         }
-        if (isset($userData['lg_status'])) {
-            $fields[] = "Lg_status = ?";
-            $params[] = $userData['lg_status'];
-        }
-        if (isset($userData['lg_timestmpin'])) {
-            $fields[] = "Lg_timestmpin = ?";
-            $params[] = $userData['lg_timestmpin'];
-        }
-        if (isset($userData['lg_timestmpiout'])) {
-            $fields[] = "Lg_timestmpiout = ?";
-            $params[] = $userData['lg_timestmpiout'];
-        }
-    
-        if (empty($fields)) {
-            return;
-        }
+        return false;
+    }
 
-        $sql = "UPDATE users SET " . implode(", ", $fields) . " WHERE id = ?";
-        $params[] = $id; 
-    
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($params);
-        }
-        
-    public function delete($id) {
-        $stmt = $this->db->prepare("DELETE FROM users WHERE id = ?");
-        $stmt->execute([$id]);
+    // List all
+    public function listAllLogs() {
+        return $this->userLogList;
     }
 }
 ?>

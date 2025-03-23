0e8673f6e5cc3f12cd5921ea5f2b95500a152bcc
sarah-frankel <frankels024@gmail.com>
2025-03-23 17:22:41 +0200

    try to preview documents

--- Back/models/mnilvim.php
+++ Back/models/mnilvim.php
@@ -4,6 +4,10 @@ class Mnilvim {
     public $MN_id;
     public $MN_pratim;
     public $MN_location;
+    public $MN_shyooch;
+    public $MN_status;
+    public $MN_dateAdd;
+    public $MN_type;
 
 }
 ?>

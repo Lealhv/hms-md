885337826ff18c1fbefba1bf14e251a7f08a4f12
lea <lea@gmail.com>
2025-03-23 17:16:40 +0200

    save reshumot

--- Back/models/reshumot.php
+++ Back/models/reshumot.php
@@ -5,11 +5,12 @@ class Reshumot {
     public $Rsh_date;
     public $Rsh_mchlaka;
     public $Rsh_sapak;
+    public $Rsh_patoor;
     public $Rsh_schoom;
     public $Rsh_maam;
     public $Rsh_schmaam;
     public $Rsh_schtotal;
-    public $Rsh_pratim;
+    public $Rsh_aspaka;
     public $Rsh_proyktnam;
     public $Rsh_status;
     public $Rsh_sochen;
